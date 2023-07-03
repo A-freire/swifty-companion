8daa4e54cf3649e47054af9ef2e1f64ac55b6bbb
Afreire- <bexas94@gmail.com>
2023-07-03 23:55:30 +0200

    poulain in grid

--- swifty/View/Piscine/PoulainView.swift
+++ swifty/View/Piscine/PoulainView.swift
@@ -9,34 +9,37 @@ import SwiftUI
 import Kingfisher
 
 struct PoulainView: View {
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+
     @State var poulain: [String: String] = [:]
     @State var showUser: Bool = false
     @State var user: User?
 
     var body: some View {
         ScrollView {
-            ForEach(Array(poulain), id: \.key) { login, image in
-                ZStack {
-                    Color.gray
-                        .cornerRadius(15)
-                    HStack {
-                        KFImage(URL(string: image))
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 42, height: 42)
-                            .clipShape(Circle())
-                        Text(login)
-                        Spacer()
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(Array(poulain), id: \.key) { login, image in
+                    ZStack {
+                        Color.gray
+                            .cornerRadius(15)
+                        VStack {
+                            KFImage(URL(string: image))
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 84, height: 84)
+                                .clipShape(Circle())
+                            Text(login)
+                        }
+                        .padding(10)
                     }
-                    .padding(10)
-                }
-                .onTapGesture {
-                    getUser(login: login)
-                }
-                .onLongPressGesture {
-                    withAnimation {
-                        poulain.removeValue(forKey: login)
-                        UserDefaults.standard.set(poulain, forKey: "poulain")
+                    .onTapGesture {
+                        getUser(login: login)
+                    }
+                    .onLongPressGesture {
+                        withAnimation {
+                            poulain.removeValue(forKey: login)
+                            UserDefaults.standard.set(poulain, forKey: "poulain")
+                        }
                     }
                 }
             }
